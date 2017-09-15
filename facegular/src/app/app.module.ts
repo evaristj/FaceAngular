9db9457bc70d21c172bff72307e63a742dab7dfd
Evarist <evaristjaumet@gmail.com>
2017-09-15 18:30:18 +0200

    cambios

--- facegular/src/app/app.module.ts
+++ facegular/src/app/app.module.ts
@@ -2,13 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PortadaComponent } from './portada/portada.component';
+import { MyPostComponent } from './my-post/my-post.component';
+import { HistorialComponent } from './historial/historial.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PrincipalComponent } from './principal/principal.component';
+import { BarranavComponent } from './barranav/barranav.component';
+
+const appRoutes: Routes = [
+  { path: 'app', component: AppComponent },
+  { path: 'my-post-component', component: MyPostComponent },
+  { path: 'portada-component', component: PortadaComponent },
+  { path: 'historial-component', component: HistorialComponent },
+  { path: '',
+    redirectTo: '/app',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortadaComponent,
+    MyPostComponent,
+    HistorialComponent,
+    PrincipalComponent,
+    BarranavComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
